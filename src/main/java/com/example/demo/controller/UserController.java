352dfe5a0e850a86bf1d7c1298f9ee1126f56dfc
bret-gibson <bret.gibson@gmail.com>
2020-11-02 21:56:32 -0500

    fixed CORS issue

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,12 +4,14 @@ import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin (origins = "http://localhost:3000")
 @RestController
 @RequestMapping("api/")
 public class UserController {
